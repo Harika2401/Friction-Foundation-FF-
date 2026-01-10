8b5b0f3259a3aa91b41cafcc35e528c68556ee21
harika0508 <harikavv2401@gmail.com>
2026-01-10 21:16:12 +0530

    first commit

---
+++ Friction-Foundation-FF--main/Frontend/src/Components/home/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+const Banner = () => {
+    return (
+        
+            
+        <div className="w-full py-2.5 font-medium text-sm text-orange-800 text-center bg-gradient-to-r from-orange-200 via-orange-100 to-orange-50">
+            <p><span className="px-3 py-1 rounded-lg text-white bg-orange-600 mr-2">New</span>Friction Foundation</p>
+        </div>
+   
+
+       
+    )
+}
+export default Banner
