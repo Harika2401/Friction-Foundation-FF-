4b20e9b799ae260fd3f4815a68d4695c8659d74a
meg-1123 <meghnareddy36789@gmail.com>
2026-01-10 19:14:18 +0530

    first commit

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react' 
+import Banner from '../Components/home/Banner'
+import Hero from '../Components/home/Hero'
 
 const Home = () => {
     return (
         <div>
-            <h1>Home Page</h1>
+            <Banner />
+            <Hero />
 
         </div>
     )
